56c165a5fad0fff4451b385cadca0e0c8482793a
Shivanandroy <snr.shivanandroy@gmail.com>
2017-10-01 17:23:00 +0530

    Add files via upload

---
+++ VIF.py
@@ -0,0 +1,34 @@
+def calculate_vif(DataFrame, thresh=5.0):
+    '''
+    DataFrame = Input Dataframe
+    thresh = Maximum threshfold for Variance Inflation Factor (VIF)
+    
+    returns:
+    
+    1. Filtered DataFrame with columns where VIF < threshold
+    2. VIF Score DataFrame
+    
+    
+    
+    '''
+    from statsmodels.stats.outliers_influence import variance_inflation_factor
+    
+    X = DataFrame.select_dtypes(include=[np.number])
+    
+    
+    variables = list(range(X.shape[1]))
+    dropped=True
+    while dropped:
+        dropped=False
+        vif = [variance_inflation_factor(X.iloc[:, variables].values, ix) for ix in range(X.iloc[:,variables].shape[1])]
+        #print('\n')
+        #print(vif)
+        
+        maxloc = vif.index(max(vif))
+        if max(vif) > thresh:
+            #print('Dropping \'' + X.iloc[:,variables].columns[maxloc] + '\' at index: ' + str(maxloc))
+            del variables[maxloc]
+            dropped=True
+    
+    #print('\n')   
+    return X.iloc[:,variables], pd.DataFrame(vif, index=X.columns[variables].tolist(), columns=['VIF'])
