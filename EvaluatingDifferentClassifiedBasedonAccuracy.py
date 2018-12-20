25e27dac068cfe39ed906a3bc030f81fc028e11f
Shivanandroy <snr.shivanandroy@gmail.com>
2018-12-20 19:55:42 +0530

    Update EvaluatingDifferentClassifiedBasedonAccuracy.py

--- EvaluatingDifferentClassifiedBasedonAccuracy.py
+++ EvaluatingDifferentClassifiedBasedonAccuracy.py
@@ -1,22 +1,28 @@
 # -*- coding: utf-8 -*-
 """
 Created on Thu Jun  8 12:23:57 2017
-
 @author: Admin
 """
+
+import warnings
+warnings.filterwarnings('ignore')
+
 
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-from sklearn.cross_validation import StratifiedShuffleSplit
+from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.metrics import accuracy_score, log_loss
 from sklearn.neighbors import KNeighborsClassifier
+from xgboost import XGBClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
 from sklearn.naive_bayes import GaussianNB
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis, QuadraticDiscriminantAnalysis
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score
+from catboost import CatBoostClassifier
 
 classifiers = [
     KNeighborsClassifier(3),
@@ -28,27 +34,30 @@ classifiers = [
     GaussianNB(),
     LinearDiscriminantAnalysis(),
     QuadraticDiscriminantAnalysis(),
-    LogisticRegression()]
+    LogisticRegression(),
+    XGBClassifier()]
 
 log_cols = ["Classifier", "Accuracy"]
 log 	 = pd.DataFrame(columns=log_cols)
 
-sss = StratifiedShuffleSplit(y, n_iter=10, test_size=0.1, random_state=0)
+#sss = StratifiedShuffleSplit(y, n_iter=10, test_size=0.1, random_state=0)
 
-X = train.drop('Survived', axis=1)
-y = train['Survived']
+X = train.drop('Attrition', axis=1)
+y = train['Attrition']
+
+sss = StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=0)
 
 acc_dict = {}
 
-for train_index, test_index in sss:
-	X_train, X_test = X[train_index], X[test_index]
-	y_train, y_test = y[train_index], y[test_index]
+for train_index, test_index in sss.split(X,y):
+	X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+	y_train, y_test = y.iloc[train_index], y.iloc[test_index]
 	
 	for clf in classifiers:
 		name = clf.__class__.__name__
 		clf.fit(X_train, y_train)
 		train_predictions = clf.predict(X_test)
-		acc = accuracy_score(y_test, train_predictions)
+		acc = roc_auc_score(y_test, train_predictions)
 		if name in acc_dict:
 			acc_dict[name] += acc
 		else:
@@ -58,9 +67,11 @@ for clf in acc_dict:
 	acc_dict[clf] = acc_dict[clf] / 10.0
 	log_entry = pd.DataFrame([[clf, acc_dict[clf]]], columns=log_cols)
 	log = log.append(log_entry)
+    
 
-plt.xlabel('Accuracy')
-plt.title('Classifier Accuracy')
-
-sns.set_color_codes("muted")
-sns.barplot(x='Accuracy', y='Classifier', data=log, color="b")
+log = log.sort_values('Accuracy')
+plt.figure(figsize=(15,8))
+plt.title('Classifier Performance')
+plt.barh(range(log.shape[0]), log.Accuracy)
+plt.yticks(range(log.shape[0]), log.Classifier)
+plt.xlabel('AUC')
